399e3a31fb6260589a645d7578f1a7261ca36510
thainatuanne <thaina-tuanne@hotmail.com>
2024-08-22 15:38:40 -0300

    Troca de imagems com JavaScript

---
+++ troca-imagem/script.js
@@ -0,0 +1,6 @@
+function changeImage () {
+    var image = document.getElementById('minha-imagem').src='imagem-2.png';
+}
+function restoreImage () {
+    var image = document.getElementById('minha-imagem').src='imagem-1.png';
+}
